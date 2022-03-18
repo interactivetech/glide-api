a2d2b62785c4b99ae2c5322e9efbe54424ae29bb
Andrew Mendez <mendeza@Andrews-MacBook-Pro.local>
2022-03-17 23:15:57 -0400

    update

--- app.py
+++ app.py
@@ -177,5 +177,5 @@ if __name__ == '__main__':
     app.logger.info("timestamp,request.remote_addr,request.method,request.scheme,request.full_path,prompt,n_images,type")
     app.run(
         host="0.0.0.0",
-        port=5000
+        port=80
     )
